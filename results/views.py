3d64cdde4c6d9d12771ff9b616c51744f9309ba5
Poshan Pandey <poshan0126@gmail.com>
2019-06-21 08:05:34 +0545

    dp
    inheritance

--- results/views.py
+++ results/views.py
@@ -56,6 +56,28 @@ def pratinidhisabha_result_details(request,pratinidhi_sabha_pk):
     }
     return render(request, template_name, context)
 
+def showDistrictSabhas(request, pradesh_no, district_name):
+    all_pradesh_sabhas = PradeshSabha.objects.all()
+    all_pratinidhi_sabhas = PratinidhiSabha.objects.all()
+    all_province = Province.objects.all()
+    all_district = District.objects.all()
+
+    province = get_object_or_404(Province, pradesh_no = pradesh_no)
+    district_name = district_name.title()
+    filter_pradesh_sabhas = PradeshSabha.objects.filter(province__name = province, district__name = district_name)
+    filter_pratinidhi_sabhas = PratinidhiSabha.objects.filter(province__name = province, district__name = district_name)
+
+    template_name = "results/filter_list.html"
+    context = {
+        "filter_pradesh_sabhas": filter_pradesh_sabhas,
+        "filter_pratinidhi_sabhas": filter_pratinidhi_sabhas,
+        "all_pradesh_sabhas": all_pradesh_sabhas,
+        "all_pratinidhi_sabhas": all_pratinidhi_sabhas,
+        "all_province": all_province,
+        "all_district": all_district,
+    }
+    return render(request, template_name, context)
+
 def pradeshsabha_result_details(request,pradeshsabha_pk):
     pradeshsabha = get_object_or_404(PradeshSabha,pk=pradeshsabha_pk)
     contesting_candidates = Nominee.objects.filter(object_id=pradeshsabha.id)
